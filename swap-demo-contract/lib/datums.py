5c4c32b47040e41bc859673b47883a8e29bd66f8
xxAVOGADROxx <jose.seraquive@gmail.com>
2024-08-05 21:13:15 -0500

    Updated datums

--- swap-demo-contract/lib/datums.py
+++ swap-demo-contract/lib/datums.py
@@ -1,18 +1,25 @@
-"""Datums implementation"""
+"""
+Datums implementation
+To gain a better understanding of the Datum Standard structure, we recommend
+visiting: https://github.com/Charli3-Official/oracle-datum-lib
+and
+https://docs.charli3.io/charli3s-documentation/oracle-feeds-datum-standard
+"""
+
 from dataclasses import dataclass
-from typing import Union, Optional
 from pycardano import PlutusData
 from pycardano.serialization import IndefiniteList
 
 
-@dataclass
-class Nothing(PlutusData):
-    CONSTR_ID = 1
+# ------------------------------#
+#         Network Feed          #
+# ------------------------------#
 
 
+# Network feed
 @dataclass
 class PriceData(PlutusData):
-    """represents cip oracle datum PriceMap(Tag +2)"""
+    """Represents cip oracle datum PriceMap(Tag +2)"""
 
     CONSTR_ID = 2
     price_map: dict
@@ -29,14 +36,77 @@ class PriceData(PlutusData):
         """get expiry of the feed"""
         return self.price_map[2]
 
-    @classmethod
-    def set_price_map(cls, price: int, timestamp: int, expiry: int):
-        """set price_map"""
-        price_map = {0: price, 1: timestamp, 2: expiry}
-        return cls(price_map)
-
 
 @dataclass
 class GenericData(PlutusData):
+    """Oracle Datum"""
+
     CONSTR_ID = 0
     price_data: PriceData
+
+
+# ------------------------------#
+#     Network Configurations    #
+# ------------------------------#
+
+
+@dataclass
+class OraclePlatform(PlutusData):
+    """Oracle Platform parameters"""
+
+    CONSTR_ID = 0
+    pmultisig_pkhs: IndefiniteList  # Allowed pkhs for platform authorization
+    pmultisig_threshold: int  # required number of signatories for authorization
+
+
+@dataclass
+class PriceRewards(PlutusData):
+    """Node Fee parameters"""
+
+    CONSTR_ID = 0
+    node_fee: int  # Individual node reward for aggregation participation.
+    aggregate_fee: int  # Node compensatoin for execution of the
+    # aggregation transaction.
+    platform_fee: int  # Platform compensation for maintenance systems.
+
+
+@dataclass
+class OracleSettings(PlutusData):
+    """Oracle Settings parameters"""
+
+    CONSTR_ID = 0
+    os_node_list: IndefiniteList  # The list of autorized nodes'
+    # public key hashes
+    os_updated_nodes: int  # The percentage of nodes needed for aggregation
+    os_updated_node_time: int  # The max time since last node update for
+    # aggregation (in milliseconds)
+    os_aggregate_time: int  # The min time since last aggregation for
+    # calculating a new one (in milliseconds)
+    os_aggregate_change: int  # The percentage of change between last
+    # aggregated value and the new one
+    os_minimum_deposit: int  # Minimum value required for topping up the
+    # aggregate UTxO (1*10^9).
+    os_aggregate_valid_range: int  # Valid time window to execute the
+    # aggregate transaction (600000, 10min)
+    os_node_fee_price: PriceRewards  # Rewards
+    os_iqr_multiplier: int  # Threshold setting 1 for Consensus:
+    # Interquartile Range (0 - N).
+    os_divergence: int  # Threshold setting 2 for Consensus:
+    # Divergence in Percentage
+    os_platform: OraclePlatform  # Oracle platform entity
+
+
+@dataclass
+class AggState(PlutusData):
+    """Agg State parameters"""
+
+    CONSTR_ID = 0
+    ag_settings: OracleSettings
+
+
+@dataclass
+class AggDatum(PlutusData):
+    """Agg Datum"""
+
+    CONSTR_ID = 2
+    aggstate: AggState
